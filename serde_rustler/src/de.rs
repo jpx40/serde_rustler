c3c879c15d3f1b2e1b753a31d4dc7f7a7c4ec39b
Jonas Peitz <jonaspeitz@proton.me>
2024-04-01 19:28:12 +0200

    version bumb

--- serde_rustler/src/de.rs
+++ serde_rustler/src/de.rs
@@ -60,26 +60,20 @@ impl<'de, 'a: 'de> de::Deserializer<'de> for Deserializer<'a> {
                 }
             }
             // i8, i16, i32, i64, u8, u16, u32, u64, f32, f64 (i128, u128)
-            TermType::Integer  {
+            TermType::Integer => {
                 try_parse_number!(self.term, u64, visitor, visit_u64);
                 try_parse_number!(self.term, i64, visitor, visit_i64);
-                try_parse_number!(self.term, f64, visitor, visit_f64);
+                //   try_parse_number!(self.term, f64, visitor, visit_f64);
 
-                           Err(Error::ExpectedNumber)
+                Err(Error::ExpectedNumber)
             }
 
-
-
-// f32, f64 
-             TermType::Float =>  {
-          
+            // f32, f64
+            TermType::Float => {
                 try_parse_number!(self.term, f64, visitor, visit_f64);
 
-                           Err(Error::ExpectedNumber)
+                Err(Error::ExpectedNumber)
             }
-
-
-
 
             // char
             // string
